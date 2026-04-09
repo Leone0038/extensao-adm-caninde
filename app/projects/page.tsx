998d648e814e5e898c88cc48af248b9171f4634a
Leone0038 <leonedantas80@gmail.com>
2026-04-08 21:51:54 -0300

    commit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,9 @@
-import ProjectCardList from "@/components/ProjectResultCardList";
 import Title from "@/components/Title";
-import { getProjects } from "@/lib/utils";
 
 export default async function ProjectsPage() {
-    const projects = await getProjects();
     return (
         <main className="page-common-styles">
             <Title text="Projetos" />
-            <ProjectCardList projects={projects} />
         </main>
     );
 }
