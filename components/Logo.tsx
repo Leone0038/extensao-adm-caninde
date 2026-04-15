84ee5b9b82561f256d4b91adae80df6782a801fd
Leone0038 <leonedantas80@gmail.com>
2026-04-14 21:39:16 -0300

    wrap logo in a transparent div

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,17 +2,25 @@ import { LogoProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Logo({ img, width, height, priority ,styles }: LogoProps) {
+export default function Logo({
+    img,
+    width,
+    height,
+    priority,
+    styles,
+}: LogoProps) {
     return (
-        <Link href="/">
-            <Image
-                src={img}
-                alt="Logo"
-                priority={priority}
-                width={width}
-                height={height}
-                className={`w-auto h-auto shrink-0 ${styles}`}
-            />
-        </Link>
+        <div className="bg-transparent">
+            <Link href="/">
+                <Image
+                    src={img}
+                    alt="Logo"
+                    priority={priority}
+                    width={width}
+                    height={height}
+                    className={`w-auto h-auto shrink-0 ${styles}`}
+                />
+            </Link>
+        </div>
     );
 }
