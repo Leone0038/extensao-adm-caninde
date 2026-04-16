4c4c3f9e9ceac81c853f14c2b06c91cfe3860186
Leone0038 <leonedantas80@gmail.com>
2026-04-16 14:37:56 -0300

    remove common link and add breadcrumbs

--- app/(main)/projects/photos/[id]/page.tsx
+++ app/(main)/projects/photos/[id]/page.tsx
@@ -1,18 +1,15 @@
-import CommonLink from "@/components/CommonLink";
+import BreadCrumbs from "@/components/BreadCrumbs";
 import PhotoList from "@/components/PhotoList";
 import { getSingleProject } from "@/lib/contentful";
 import {  ProjectSubPageProps } from "@/types";
 
 export default async function PhotosPage({ params }: ProjectSubPageProps) {
     const { id } = await params;
-    const { photos } = await getSingleProject(id);
+    const { photos, title } = await getSingleProject(id);
     return (
         <main className="page-common-styles">
-            <CommonLink
-                href={`/projetos/${id}`}
-                label="Voltar"
-                styles="mb-10 w-30 text-xl"
-            />
+           
+            <BreadCrumbs projectName={title}/>
             <PhotoList photos={photos} />
         </main>
     );
