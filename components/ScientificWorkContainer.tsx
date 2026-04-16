f8e292db68d90861c09541c7d46b00e1c25def9a
Leone0038 <leonedantas80@gmail.com>
2026-04-16 14:51:10 -0300

    add newly created css class

--- components/ScientificWorkContainer.tsx
+++ components/ScientificWorkContainer.tsx
@@ -4,5 +4,5 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 export default function ScientificWorkContainer({
     scientificWorks,
 }: ScientificWorkContainerProps) {
-    return <div className="[&_a]:underline p-4 break-all flex flex-col gap-4 text-bg">{documentToReactComponents(scientificWorks)}</div>;
+    return <div className="document-style text-bg">{documentToReactComponents(scientificWorks)}</div>;
 }
--- components/VideoLinks.tsx
+++ components/VideoLinks.tsx
@@ -3,7 +3,7 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export default function VideoLinks({ videoLinks }: VideoLinkProps) {
     return (
-        <div className="[&_a]:underline break-all flex flex-col gap-4 text-bg">
+        <div className="document-style text-bg">
             {documentToReactComponents(videoLinks)}
         </div>
     );
