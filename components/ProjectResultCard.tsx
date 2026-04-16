6d87c4548edee9d1e2928ca5c029e769e208302d
Leone0038 <leonedantas80@gmail.com>
2026-04-16 14:51:02 -0300

    add newly created css class

--- components/ProjectResultCard.tsx
+++ components/ProjectResultCard.tsx
@@ -2,5 +2,5 @@ import { ProjectResultCardProps } from "@/types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default function ProjectResultCard({ results }: ProjectResultCardProps) {
-    return <div className="[&_a]:underline break-all w-4/5 p-4 flex flex-col gap-4 text-bg">{documentToReactComponents(results)}</div>;
+    return <div className="document-style text-bg">{documentToReactComponents(results)}</div>;
 }
