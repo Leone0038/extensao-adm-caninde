6d75bc4af0c9f243a0856f4830c3aeee59ff1ac8
Leone0038 <leonedantas80@gmail.com>
2026-04-16 14:39:18 -0300

    remove padding

--- components/VideoLinks.tsx
+++ components/VideoLinks.tsx
@@ -3,7 +3,7 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export default function VideoLinks({ videoLinks }: VideoLinkProps) {
     return (
-        <div className="[&_a]:underline p-4 break-all flex flex-col gap-4 text-bg">
+        <div className="[&_a]:underline break-all flex flex-col gap-4 text-bg">
             {documentToReactComponents(videoLinks)}
         </div>
     );
