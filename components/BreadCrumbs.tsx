3512dcd20474550cac204d6ea345ca96d35aad44
Leone0038 <leonedantas80@gmail.com>
2026-04-17 00:05:39 -0300

    change logic

--- components/BreadCrumbs.tsx
+++ components/BreadCrumbs.tsx
@@ -4,17 +4,32 @@ import { BreadCrumbsProps } from "@/types";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function BreadCrumbs({ projectName }: BreadCrumbsProps) {
+export default function BreadCrumbs({ projectName, year }: BreadCrumbsProps) {
     const pathname = usePathname();
-    const pathSegments = pathname
+    let pathSegments = pathname
         .split("/")
         .filter((p) => p !== "projetos" && p !== "");
+    if (year === 2025) {
+        pathSegments.unshift("projetos-lista-2025");
+    } else if (year === 2026) {
+        pathSegments.unshift("projetos-lista-2026");
+    } else if (
+        pathSegments.includes("fotos") ||
+        pathSegments.includes("videos") ||
+        pathSegments.includes("resultados") ||
+        pathSegments.includes("trabalhos")
+    ) {
+        pathSegments = pathSegments.reverse();
+    }
     const labelMap: Record<string, string> = {
         projetos: "Projetos",
         fotos: "Fotografias",
         videos: "Vídeos",
         resultados: "Produtos da ação",
         trabalhos: "Trabalhos científicos",
+        sobre: "Sobre",
+        "projetos-lista-2025": "Projetos de Extensão 2025",
+        "projetos-lista-2026": "Projetos de Extensão 2026",
     };
     return (
         <nav className="pb-8 font-medium text-center [&_a]:hover:underline">
@@ -22,13 +37,17 @@ export default function BreadCrumbs({ projectName }: BreadCrumbsProps) {
                 <li>
                     <Link href="/">Início</Link>
                 </li>
-                {pathSegments.reverse().map((segment, i) => {
+                {pathSegments.map((segment, i) => {
                     const isLast = i === pathSegments.length - 1;
-                    const isID = segment.length > 15;
+                    const isID =
+                        segment.length > 15 && !segment.includes("projetos");
                     const href = `/${pathSegments.slice(0, i + 1).join("/")}`;
-                    const displayLabel = isID ? projectName : labelMap[segment];
+                    const displayLabel = isID ? projectName?.slice(0, 30) + '...' : labelMap[segment];
                     return (
-                        <li key={i} className="flex items-center gap-2">
+                        <li
+                            key={i}
+                            className="flex items-center gap-2"
+                        >
                             <span>/</span>
                             {isLast ? (
                                 <span> {displayLabel} </span>
