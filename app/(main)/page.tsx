057a65b3207c5e7cd0b84b757f9e62943244c6cc
Leone0038 <leonedantas80@gmail.com>
2026-04-17 00:03:26 -0300

    remove presentation component and add image

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,21 +1,30 @@
 import CommentCardList from "@/components/CommentCardList";
 import CommentForm from "@/components/CommentForm";
-import Presentation from "@/components/Presentation";
 import SideMenu from "@/components/SideMenu";
 import Title from "@/components/Title";
+import Image from "next/image";
+import newsImage from "@/public/images/news.webp";
+import Link from "next/link";
 
 export default async function Home() {
     return (
         <main className="flex flex-col sm:items-start items-center gap-8">
-            <Title text="Sobre o Site"/>
-            <div className="grid lg:grid-cols-3 grid-cols-1 gap-12 mb-10">
-                <Presentation />
+            <Title text="Últimas notícias" />
+            <div className="grid lg:grid-cols-3 grid-cols-1 gap-12 mb-10 w-full">
+                <Link href={"/sobre"} className="lg:col-span-2">
+                    <Image
+                        width={1024}
+                        height={1024}
+                        alt="Notícias"
+                        src={newsImage}
+                        className="w-full h-full"
+                        priority
+                    />
+                </Link>
                 <SideMenu />
             </div>
             <div className="flex flex-col gap-4 p-4 max-w-4xl w-full">
-                <Title
-                    text="Deixe seu comentário sobre os projetos de extensão do curso de Administração"
-                />
+                <Title text="Deixe seu comentário sobre os projetos de extensão do curso de Administração" />
 
                 <CommentForm />
                 <CommentCardList />
