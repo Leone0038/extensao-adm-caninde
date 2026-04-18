ab36e340f9433b05e02a149f46669e41e31218c8
Leone0038 <leonedantas80@gmail.com>
2026-04-18 13:35:42 -0300

    tweak list

--- components/PhotoList.tsx
+++ components/PhotoList.tsx
@@ -1,16 +1,41 @@
+"use client";
+
 import { PhotoListProps } from "@/types";
 import PhotoCard from "./PhotoCard";
 import NoItemsNotice from "./NoItemsNotice";
+import { useState } from "react";
+import Modal from "./Modal";
 
 export default function PhotoList({ photos }: PhotoListProps) {
-    let processedPhotos;
-    if (photos) {
-        processedPhotos = photos?.map((photoUrl, i) => (
-            <PhotoCard photoUrl={photoUrl.fields.file.url} index={i} key={i} />
-        ));
-    } else {
-        processedPhotos = <NoItemsNotice text="Nenhuma foto postada ainda" />;
+    const [showModal, setShowModal] = useState(false);
+    const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+
+    if (!photos || photos.length === 0) {
+        return <NoItemsNotice text="Nenhuma foto postada ainda" />;
     }
-  
-    return <ul className="grid-common-styles mx-4">{processedPhotos}</ul>;
+
+    return (
+        <>
+            <ul className="grid-common-styles mx-4">
+                {photos.map((photo, i) => (
+                    <PhotoCard
+                        key={i}
+                        photoUrl={photo.fields.file.url}
+                        index={i}
+                        openModal={(idx) => {
+                            setCurrentPhotoIndex(idx);
+                            setShowModal(true);
+                        }}
+                    />
+                ))}
+            </ul>
+            {showModal && (
+                <Modal
+                    closeModal={() => setShowModal(false)}
+                    currentPhotoIndex={currentPhotoIndex}
+                    photos={photos}
+                />
+            )}
+        </>
+    );
 }
