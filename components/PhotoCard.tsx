109dc84cfd8dccb5fe55206925c5df42dc6989bd
Leone0038 <leonedantas80@gmail.com>
2026-04-18 13:36:17 -0300

    tweak photo card

--- components/PhotoCard.tsx
+++ components/PhotoCard.tsx
@@ -1,28 +1,25 @@
-"use client";
-
 import { cleanUrl } from "@/lib/utils";
 import { PhotoCardProps } from "@/types";
 
 import Image from "next/image";
-import Link from "next/link";
-import { useParams } from "next/navigation";
-const allowedIndexes = [0, 1, 2, 3, 4];
 
-export default function PhotoCard({ photoUrl, index }: PhotoCardProps) {
+export default function PhotoCard({
+    photoUrl,
+    index,
+    openModal,
+}: PhotoCardProps) {
     const imageUrl = cleanUrl(photoUrl);
-    const { id } = useParams();
     return (
-        <li id={id?.toString()}>
-            <Link href={`/projetos/slideshow/${id}?index=${index}`}>
-                <Image
-                    width={300}
-                    height={300}
-                    src={imageUrl}
-                    priority={allowedIndexes.includes(index)}
-                    alt={"Foto do projeto"}
-                    className="object-cover text-bg rounded aspect-square"
-                />
-            </Link>
+        <li className="relative aspect-square rounded">
+            <Image
+                fill
+                src={imageUrl}
+                priority={index < 4}
+                onClick={() => openModal(index)}
+                alt={"Foto do projeto"}
+                className="object-cover rounded aspect-square cursor-pointer"
+                sizes="100vw"
+            />
         </li>
     );
 }
