633a05b50d0f9fed8e61a9c56b531a141f5a6501
Leone0038 <leonedantas80@gmail.com>
2026-04-19 13:56:25 -0300

    add external video component

--- app/(main)/projects/videos/[id]/page.tsx
+++ app/(main)/projects/videos/[id]/page.tsx
@@ -1,15 +1,17 @@
 import BreadCrumbs from "@/components/BreadCrumbs";
 import VideoLinks from "@/components/VideoLinks";
+import VideoList from "@/components/VideoList";
 import { getSingleProject } from "@/lib/contentful";
 import { ProjectSubPageProps } from "@/types";
 
 export default async function VideosPage({ params }: ProjectSubPageProps) {
     const { id } = await params;
-    const { videos , title} = await getSingleProject(id);
+    const { videos, title, videoLinks } = await getSingleProject(id);
     return (
         <main className="flex-1">
             <BreadCrumbs projectName={title} />
-            <VideoLinks videoLinks={videos} />
+            <VideoLinks videoLinks={videoLinks} />
+            <VideoList videos={videos} />
         </main>
     );
 }
