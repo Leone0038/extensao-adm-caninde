554e37dfd0a90d66f9c9b578bb1e75784b1e886a
Leone0038 <leonedantas80@gmail.com>
2026-04-19 13:56:59 -0300

    add margin

--- components/NoItemsNotice.tsx
+++ components/NoItemsNotice.tsx
@@ -1,5 +1,5 @@
 import { NoItemsNoticeProps } from "@/types";
 
 export default function NoItemsNotice({text}:NoItemsNoticeProps) {
-    return <p className="text-2xl italic text-gray-500">{text}</p>;
+    return <p className="text-2xl italic text-gray-500 m-4">{text}</p>;
 }
