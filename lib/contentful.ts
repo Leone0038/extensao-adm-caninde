61dae9c2ef75dfce95bd5b6c8b13a962b7d1160b
Leone0038 <leonedantas80@gmail.com>
2026-04-21 13:43:37 -0300

    remove unused functions

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -1,68 +1,10 @@
-import { FilterType, Project } from "@/types";
+import { Project } from "@/types";
 import { createClient } from "contentful";
 
 export const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID!,
     accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN!,
 });
-
-
-export const getNews = async (
-    search: string = "",
-    filter: FilterType = "latest",
-    limit: number,
-    currentPage: number,
-) => {
-    const response = await client.getEntries({
-        content_type: "news",
-        query: search,
-        order: [filter === "latest" ? "-sys.createdAt" : "sys.createdAt"],
-        limit,
-        skip: (currentPage - 1) * limit,
-    });
-    return { news: response.items, total: response.total };
-};
-
-export const getLastestNews = async () => {
-    const response = await client.getEntries({
-        content_type: "news",
-        limit: 3,
-        order: ["-sys.createdAt"],
-    });
-    return response.items;
-};
-
-export const getCounter = async () => {
-    const response = await client.getEntries({
-        content_type: "counter",
-    });
-    return response.items;
-};
-
-
-export const getPhotos = async () => {
-    const response = await client.getEntries({
-        content_type: "photos",
-    });
-    return response.items;
-};
-
-export const getVideos = async () => {
-    const response = await client.getEntries({
-        content_type: "vdeos",
-    });
-    return response.items;
-};
-
-export const getScientificWorks = async () => {
-    const response = await client.getEntries({
-        content_type: "scientificWork",
-    });
-
-    return response.items;
-};
-
-
 
 export const getProjects = async () => {
     const response = await client.getEntries({
@@ -77,7 +19,6 @@ export const getSingleProject = async (id: string) => {
 
     return response.fields as unknown as Promise<Project>;
 };
-
 
 export const getCourses = async () => {
     const response = await client.getEntries({
