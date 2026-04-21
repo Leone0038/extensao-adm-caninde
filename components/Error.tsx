7fe4f45894995bd5c270d12b6193e667d0e070dd
Leone0038 <leonedantas80@gmail.com>
2026-04-21 13:42:21 -0300

    change text color

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,5 +1,5 @@
 export default function Error() {
     return (
-        <div className="bg-red-500 p-4 rounded shadow border">Ocorreu um erro</div>
+        <div className="bg-red-500 p-4 rounded shadow border text-text-primary">Ocorreu um erro</div>
     );
 }
